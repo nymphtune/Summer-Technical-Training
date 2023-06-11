7b937ec361dd6cc1fd746b030e45e6e28f1b06d0
Shreya Umrao <91051605+nymphtune@users.noreply.github.com>
2023-06-11 23:23:10 +0530

    Update Two.java

--- RecursionPatternsMultiBranch/Two.java
+++ RecursionPatternsMultiBranch/Two.java
@@ -1,1 +1,20 @@
+package RecursionPatternsMultiBranch;
 
+import java.util.*;
+
+class Two {
+     public static int climb(int n) {
+          if (n <= 2) {
+               return n;
+          }
+          return climb(n - 1) + climb(n - 2);
+     }
+
+     public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Enter the number of stairs:");
+          int stairs = sc.nextInt();
+          System.out.println("Number of ways to climb " + stairs + " stairs: " + climb(stairs));
+          sc.close();
+     }
+}
